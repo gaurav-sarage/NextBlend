5ab0dcf28802fc2c500af42f830300b483ddc900
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-02-29 19:45:04 +0530

    Imported Hero component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Banner from "./components/Banner";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Hero />
     </div>
   )
 };
