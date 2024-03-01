8aeb01122b27db35f0e858e5e6a1d93ec393e2cb
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-01 22:29:02 +0530

    Changed content

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -5,7 +5,7 @@ export default function Banner() {
         <div className="bg-indigo-600">
             <div className="max-w-screen-xl mx-auto px-4 py-3 items-center gap-x-4 justify-center text-white sm:flex md:px-8">
                 <p className="py-2 font-medium">
-                    NextBlend is currently under development!  🚧🚧 Come back later for a fully functional website. ⏰
+                🚧🚧 NextBlend is currently under development!  🚧🚧
                 </p>
                 <Link href="/" className="flex-none inline-block w-full mt-3 py-2 px-3 text-center text-indigo-600 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 rounded-lg sm:w-auto sm:mt-0 sm:text-sm">
                     Learn more
