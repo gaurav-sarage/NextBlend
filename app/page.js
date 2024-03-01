f546123e3cb4397430f7fa44706cfb0a70e55125
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-01 22:33:53 +0530

    Imported CallToAction component

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Toolkit from "./components/Toolkit";
+import CallToAction from "./components/CTA";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Toolkit />
+      <CallToAction />
     </div>
   )
 };
