3bd0e68b96b00c414b6ba89db37d20178937bfcc
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-05 23:03:17 +0530

    Added dummy pages, checking for routing

---
+++ app/(pages)/about/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/app/components/Navbar";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+};
+
+export default AboutPage;
---
+++ app/(pages)/components/page.js
@@ -0,0 +1,11 @@
+const ComponentPage = () => {
+    return (
+        <div>
+            <p className="text-center">
+                Hello from the Component page
+            </p>
+        </div>
+    )
+};
+
+export default ComponentPage;
