a3e8ba3cc1d9b377031d908d902c8183de875926
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-19 05:05:03 +0530

    Finished building the CTA section

--- app/components/CTA.js
+++ app/components/CTA.js
@@ -1,28 +1,37 @@
-import Link from "next/link";
+import React from "react";
 
-const CallToAction = () => {
+const CTA = () => {
     return (
-        <section className="mt-16">
-            <div className="max-w-screen-xl mx-auto px-4 md:text-center md:px-8">
-                <div className="max-w-xl md:mx-auto">
-                    <h2 className="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
-                        Make your ideas come to life <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]">with no time</span>
+        <div className="mx-auto max-w-7xl px-2 py-10 lg:px-0">
+            <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
+                <div className="w-full md:w-1/2">
+                    <h2 className="text-3xl font-bold text-black px-10">
+                        Get updates straight to your inbox
                     </h2>
-                    <p className="mt-3 text-black text-lg">
-                        Copy and paste our ready to use components into your apps, and save time!
+                    <p className="mt-2 text-black hover:text-indigo-600 px-16">
+                        Lorem ipsem dolor sit amet, consectur adipiscing elit.
                     </p>
                 </div>
-                <div className="flex gap-3 items-center mt-4 md:justify-center">
-                    <Link href="/components" className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
-                        Get Started
-                    </Link>
-                    <Link href="/" className="block py-2 px-4 text-gray-700 hover:text-white font-medium duration-150 hover:bg-black active:bg-gray-100 border rounded-lg">
-                        Learn more
-                    </Link>
+                <div className="mt-10 w-full md:w-1/2 lg:mt-0">
+                    <form className="flex px-12 lg:justify-center">
+                        <div className="flex w-full max-w-md items-center space-x-2">
+                            <input
+                                className="flex h-10 w-full rounded-lg border border-black/30 bg-transparent px-3 py-3 text-sm placeholder:text-black"
+                                type="email"
+                                placeholder="Enter your email"
+                            ></input>
+                            <button
+                                type="button"
+                                className="rounded-lg bg-black hover:bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-black"
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </section>
+        </div>
     );
 };
 
-export default CallToAction;
+export default CTA;
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => {
                             <div className="px-8 sm:items-center sm:justify-center sm:px-0 sm:space-x-5 sm:flex mt-9">
                                 <Link
                                     href="/components"
-                                    className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-xl hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                                    className="inline-flex items-center justify-center w-full px-8 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 border-2 border-transparent sm:w-auto rounded-full hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                                     role="button"
                                 >
                                     Browse Components
@@ -32,7 +32,7 @@ const Hero = () => {
 
                                 <Link
                                     href="/custom-components"
-                                    className="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-900 transition-all duration-200 border-2 sm:w-auto sm:mt-0 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-indigo-600 focus:bg-indigo-900 hover:text-white focus:text-white"
+                                    className="inline-flex items-center justify-center w-full px-6 py-3 mt-4 text-lg font-bold text-gray-900 transition-all duration-200 border-2 sm:w-auto sm:mt-0 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 hover:bg-indigo-600 focus:bg-indigo-900 hover:text-white focus:text-white"
                                 >
                                     Custom Components
                                 </Link>
