4bdf2bab58d2fc703457665f8e27fe33cf105aac
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-18 22:40:45 +0530

    Revamping the Hero component

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -3,28 +3,18 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <section>
-            <div className="max-w-screen-xl mx-auto px-4 py-24 gap-12 text-gray-600 md:px-8">
-                <div className="space-y-5 max-w-4xl mx-auto text-center">
-                    <h1 className="text-lg text-indigo-600 font-medium uppercase">
-                        Build products for everyone
-                    </h1>
-                    <h2 className="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
-                    Accelerate frontend development using our comprehensive <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]">selection of free UI components.</span>
-                    </h2>
-
-                    <div className="items-center py-4 justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-                        <Link href="/components" className="block py-2 px-4 text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
-                            Browse Components
-                        </Link>
-                        <Link href="https://github.com/gaurav-sarage/NextBlend" target="_blank" className="block py-2 px-4 text-gray-700 hover:text-white font-medium duration-150 hover:bg-black active:bg-gray-100 border rounded-lg">
-                            Star on GitHub
-                        </Link>
+        <div className="overflow-x-hidden bg-gray-50">
+            <section className="pt-12 bg-gray-50 sm:pt-16">
+                <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="max-w-2xl mx-auto text-center">
+                        <h1 className="px-6 text-lg text-black font-semibold">
+                            Build products for everyone
+                        </h1>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        </div>
+    );
 };
 
 export default Hero;
