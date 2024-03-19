1323a4d86ca44eacb620de9b65c59f739e01ce25
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-19 06:49:33 +0530

    Imported Banner

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import CallToAction from "./components/CTA";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
     <div>
+      <Banner />
       <Navbar />
       <Hero />
       <CallToAction />
