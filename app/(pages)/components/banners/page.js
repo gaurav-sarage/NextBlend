d1cc03fcf1bfae4ff18bb6b4ba9ef65b3fbabc59
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-03-24 15:04:18 +0530

    🎉 Implemented `Centered Banner with Badge` component

--- app/(pages)/components/banners/page.js
+++ app/(pages)/components/banners/page.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
+
+import React from "react";
+
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
-import React from "react";
 import Sidebar from "@/app/components/Sidebar";
 
 const ListBannerComponents = () => {
@@ -17,11 +19,11 @@ const ListBannerComponents = () => {
                     </div>
 
                     <div className="grid grid-cols-2 gap-6 mt-6 lg:mt-10 lg:gap-6 lg:grid-cols-3">
-                        {/* Centered Banner */}
+                        {/* Centered Banner with Underline Link*/}
                         <div className="relative group">
                             <div className="overflow-hidden aspect-w-1 aspect-h-1">
                                 <Link href="/components/banners/centered-banner-link-underline">
-                                    <img className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125" src="/tabs.png" alt="Banner Centered Image" />
+                                    <img className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125" src="/tabs.png" alt="Banner Centered with Underline Link Image" />
                                 </Link>
                             </div>
                             <div className="absolute left-0 right-0 bottom-0 top-auto bg-gradient-to-t from-transparent to-black text-white text-center p-2">
@@ -36,6 +38,24 @@ const ListBannerComponents = () => {
                             </div>
                         </div>
 
+                        {/* Centered Banner with Badge */}
+                        <div className="relative group">
+                            <div className="overflow-hidden aspect-w-1 aspect-h-1">
+                                <Link href="/components/banners/centered-banner-badge">
+                                    <img className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125" src="/tabs.png" alt="Banner Centered with Badge Image" />
+                                </Link>
+                            </div>
+                            <div className="absolute left-0 right-0 bottom-0 top-auto bg-gradient-to-t from-transparent to-black text-white text-center p-2">
+                                <h3 className="text-xs font-bold hover:text-indigo-600 sm:text-sm md:text-base cursor-pointer">
+                                    Centered Banner with Badge
+                                </h3>
+                            </div>
+                            <div className="absolute left-3 top-3">
+                                <p className="sm:px-3 sm:py-1.5 px-1.5 py-1 text-[8px] sm:text-xs font-bold tracking-wide text-white uppercase bg-gray-900 rounded-full">
+                                    Newly Launched
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
